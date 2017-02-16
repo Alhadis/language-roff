e4d3282892618edbb4e62d560fdfc72347aa4ba1
Alhadis <gardnerjohng@gmail.com>
2017-02-17 07:19:33 +1100

    Add commands for converting Roff to other formats

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const {CompositeDisposable} = require("atom");
+const {join, parse} = require("path");
+const fs = require("fs");
+const Groff = require("./lib/groff.js");
 
 
 class LanguageRoff{
@@ -8,13 +11,30 @@ class LanguageRoff{
 	activate(){
 		this.disposables = new CompositeDisposable();
 		
-		/** Register commands with Atom */
-		const selector =
-			"atom-text-editor[data-grammar^='text roff']," +
-			"atom-text-editor[data-grammar^='text runoff']";
+		// Register commands with Atom
+		let selector = "atom-text-editor[data-grammar='text roff']";
+		this.disposables.add(atom.commands.add(selector, {
+			"language-roff:indent":  () => this.indent(),
+			"language-roff:outdent": () => this.outdent()
+		}));
 		
-		this.disposables.add(atom.commands.add(selector, "language-roff:indent",  _=> this.indent()));
-		this.disposables.add(atom.commands.add(selector, "language-roff:outdent", _=> this.outdent()));
+		selector += ", atom-text-editor[data-grammar='source pic']";
+		this.disposables.add(atom.commands.add(selector, {
+			"language-roff:save-as-dvi":  () => this.saveEditor("dvi"),
+			"language-roff:save-as-html": () => this.saveEditor("html"),
+			"language-roff:save-as-pdf":  () => this.saveEditor("pdf"),
+			"language-roff:save-as-ps":   () => this.saveEditor("ps")
+		}));
+		
+		selector = ".tree-view .file > .manpage-icon[data-path], .tab > .manpage-icon[data-path]";
+		for(const ext of "1 2 3 4 5 6 7 8 9 chem man mdoc me ms n pic roff tr".split(/\s/))
+			selector += `, .tree-view .file > [data-path$=".${ext}"], .tab > [data-path$=".${ext}"]`;
+		this.disposables.add(atom.commands.add(selector, {
+			"language-roff:save-as-dvi":  ev => this.saveEntry(ev, "dvi"),
+			"language-roff:save-as-html": ev => this.saveEntry(ev, "html"),
+			"language-roff:save-as-pdf":  ev => this.saveEntry(ev, "pdf"),
+			"language-roff:save-as-ps":   ev => this.saveEntry(ev, "ps")
+		}));
 	}
 	
 
@@ -93,6 +113,41 @@ class LanguageRoff{
 				}
 			}
 		});
+	}
+	
+	
+	saveEditor(format, editor = null){
+		editor = editor || atom.workspace.getActiveTextEditor();
+		if(!editor) return;
+		
+		const path = editor.getPath();
+		const target = this.getSaveLocation(path, format);
+		if(!target) return;
+		
+		return path && !editor.isModified()
+			? Groff.processFile(path, target, format)
+			: Groff.processData(editor.getText(), target, format);
+	}
+	
+	
+	saveEntry(event, format){
+		if(!(event.target && event.target.dataset)) return;
+		const {path} = event.target.dataset;
+		const target = this.getSaveLocation(path, format);
+		if(!target) return;
+		Groff.processFile(path, target, format);
+	}
+	
+	
+	getSaveLocation(path, format){
+		if(path){
+			const {dir, name} = parse(path);
+			path = join(dir, `${name}.${format}`);
+		}
+		else if(path = atom.project.getPaths()[0])
+			path = join(path, "Untitled." + format);
+		
+		return atom.applicationDelegate.showSaveDialog({defaultPath: path});
 	}
 }
 
