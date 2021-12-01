757a8681df699d724bc8ff0f852f73118f06c584
Alhadis <gardnerjohng@gmail.com>
2021-12-01 12:11:35 +1100

    Regenerate fixtures

--- spec/grammar-spec.js
+++ spec/grammar-spec.js
@@ -5,12 +5,14 @@ const fs = require("fs");
 
 describe("Syntax highlighting", function(){
 	this.timeout(30000);
-	let roff;
+	let roff, ditroff, pic;
 	
 	before(async () => {
 		await atom.packages.activatePackage("language-hyperlink");
 		const pkg = await atom.packages.activatePackage(resolve(__dirname, ".."));
-		roff = pkg.grammars.find(g => "text.roff" === g.scopeName);
+		roff    = pkg.grammars.find(g => g.scopeName === "text.roff");
+		ditroff = pkg.grammars.find(g => g.scopeName === "source.ditroff");
+		pic     = pkg.grammars.find(g => g.scopeName === "source.pic");
 	});
 	
 	const inputDir  = join(__dirname, "fixtures", "input");
@@ -19,7 +21,10 @@ describe("Syntax highlighting", function(){
 	for(const filename of files){
 		const input  = fs.readFileSync(join(inputDir, filename), "utf8").replace(/\n+$/, "");
 		const output = fs.readFileSync(join(outputDir, filename + ".json"), "utf8");
-		it(`tokenises ${filename}`, () => roff.tokenizeLines(input).should.eql(
-			JSON.parse(output).map(l => l.map(([value, ...scopes]) => ({value, scopes})))));
+		it(`tokenises ${filename}`, () => {
+			const grammar = /\.(?:pic|pikchr)$/i.test(filename) ? pic : filename.startsWith("ditroff.") ? ditroff : roff;
+			const savedTokens = JSON.parse(output).map(l => l.map(([value, ...scopes]) => ({value, scopes})));
+			grammar.tokenizeLines(input).should.eql(savedTokens);
+		});
 	}
 });
